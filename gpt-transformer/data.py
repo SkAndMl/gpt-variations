fixed data leakage

gpt-transformer/data.py
@@ -1,8 +1,8 @@
 from torch.utils.data import Dataset
 from tokenizers import Tokenizer
-from datasets import load_dataset
 import torch
 import random
+import os
 
 class Tokens:
     pad_token = 0
@@ -10,27 +10,41 @@ class Tokens:
     eos_token = 2
     unk_token = 3
     sep_token = 4
+    
+
+def split_data(lang1: str, lang2: str, test_size: float=0.1) -> None:
+
+    with open(f"{lang1}-{lang2}.txt", "r") as f:
+        data = f.readlines()
+        data = [d.strip() for d in data]
+    
+    random.seed(2406)
+    random.shuffle(data)
+
+    split_idx = int(len(data)*test_size)
+    with open(f"{lang1}-{lang2}-test.txt", "w") as f:
+        for d in data[:split_idx]:
+            f.write(d+"\n")
+    
+    with open(f"{lang1}-{lang2}-train.txt", "w") as f:
+        for d in data[split_idx:]:
+            f.write(d+"\n")
 
 class LangDataset(Dataset):
 
-    def __init__(self, lang1: str, lang2: str, split: str="train", test_size: int=0.1) -> None:
+    def __init__(self, lang1: str, lang2: str, split: str="train", test_size: float=0.1) -> None:
         super().__init__()
 
         self.lang1 = lang1
         self.lang2 = lang2
         self.tokenizer: Tokenizer = Tokenizer.from_file(f"{lang1}-{lang2}.json")
         
-        with open(f"{lang1}-{lang2}.txt", "r") as f:
+        if not os.path.exists(f"{lang1}-{lang2}-{split}.txt"):
+            split_data(lang1, lang2, test_size)
+        
+        with open(f"{lang1}-{lang2}-{split}.txt", "r") as f:
             data = f.readlines()
             data = [d.strip() for d in data]
-            
-            random.seed(2406)
-            if split=="train":
-                idxs = random.sample(list(range(len(data))), k=int(len(data)*(1-test_size)))
-            else:
-                idxs = random.sample(list(range(len(data))), k=int(len(data)*test_size))
-            
-            data = [data[idx] for idx in idxs]
         
         self.data = data
     

gpt-transformer/train.py
@@ -2,18 +2,24 @@ from data import LangDataset
 from tokenizers import Tokenizer
 import json
 from scripts import Tokens, initialize_weights
-from translate_former import PosTranslateFormer
+from translate_former import PosTranslateFormer, ConvTranslateFormer
+from translate_former import ParallelTranslateFormer, TranslateFormer
 import torch
 from torch import nn as nn
 from torch.optim import Adam
 from contextlib import nullcontext
 from torch.cuda.amp import autocast, GradScaler
 from torch.nn.utils.rnn import pad_sequence
+from torchmetrics.text import BLEUScore, WordErrorRate, CharErrorRate
 from torch.utils.tensorboard import SummaryWriter
-from datetime import datetime
 import logging
+import random
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+model_name = "translateformer"
+log_dir = f'tensorboard_results/{model_name}'
+writer = SummaryWriter(log_dir)
 
 with open("config.json", "r") as f:
     config = json.loads(f.read())
@@ -34,10 +40,77 @@ eval_step = 1000
 eval_steps = 1000
 gradient_accumulation_steps = 2
 
-model = PosTranslateFormer(config=config).to(config["device"])
+if model_name.startswith("pos"):
+    model = PosTranslateFormer(config=config)
+elif model_name.startswith("conv"):
+    model = ConvTranslateFormer(config=config)
+elif model_name.startswith("par"):
+    model = ParallelTranslateFormer(config=config)
+else:
+    model = TranslateFormer(config=config)
+
+model = model.to(config["device"])
 model.apply(initialize_weights)
 logging.info("Initialized model")
 optimizer = Adam(params=model.parameters(), lr=config["initial_lr"], weight_decay=config["weight_decay"])
+
+bleu = BLEUScore()
+wer = WordErrorRate()
+cer = CharErrorRate()
+
+
+def log_results(train_loss: float, eval_loss: float, step: int) -> None:
+
+    rand_idx = random.sample(range(0, len(test_ds)), k=1)[0]
+    sample: str = test_ds.data[rand_idx]
+    sep_idx = sample.find("<sep>")
+    
+    src, tgt = sample[5:sep_idx], sample[sep_idx+5:-5]
+    src, tgt = src.strip(), tgt.strip()
+
+    pred = model.translate(x=src)
+    
+    bleu.update([pred], [[tgt]])
+    wer.update([pred], [tgt])
+    cer.update([pred], [tgt])
+
+    bleu_score: float = bleu.compute().item()
+    wer_score: float = wer.compute().item()
+    cer_score: float = cer.compute().item()
+
+    bleu.reset()
+    wer.reset()
+    cer.reset()
+
+    ## add losses to tensorboard
+    writer.add_scalar(
+        tag = "Loss/train", 
+        scalar_value=train_loss,
+        global_step=step
+    )
+    writer.add_scalar(
+        tag = "Loss/test", 
+        scalar_value=eval_loss,
+        global_step=step
+    )
+
+    ## add metrics to tensorboard
+    writer.add_scalar(
+        tag="Metric/bleu",
+        scalar_value=bleu_score,
+        global_step=step
+    )
+    writer.add_scalar(
+        tag="Metric/wer",
+        scalar_value=wer_score,
+        global_step=step
+    )
+    writer.add_scalar(
+        tag="Metric/cer",
+        scalar_value=cer_score,
+        global_step=step
+    )
+
 
 def save_checkpoint(state, filename="checkpoint.pt"):
     torch.save(state, filename)
@@ -66,10 +139,6 @@ def eval_model() -> float:
 
 def train():
     train_loss = 0
-    current_time = datetime.now().strftime('%Y%m%d-%H%M%S')
-    log_dir = f'runs/translate_former_experiment_{current_time}'
-    writer = SummaryWriter(log_dir)
-
     best_eval_loss = float("inf")
 
     model.train()
@@ -101,11 +170,10 @@ def train():
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
                     'loss': eval_loss,
-                }, filename=f'checkpoint_{current_time}.pt')
+                }, filename=f'{model_name}_cp.pt')
 
 
-            writer.add_scalar('Loss/train', train_loss, step)
-            writer.add_scalar('Loss/eval', eval_loss, step)
+            log_results(train_loss, eval_loss, step)
 
             print(f"({step*100/train_steps:.2f}%) train: {train_loss:.4f} eval: {eval_loss:.4f}")
             train_loss = 0

gpt-transformer/translate_former.py
@@ -38,22 +38,24 @@ class TranslateFormer(nn.Module):
         return logits, loss
     
     @torch.inference_mode()
-    def translate(self, x: str, max_new_tokens: int=20) -> str:
+    def translate(self, x: str, max_len: int=20) -> str:
         x = "<sos>" + x + "<sep>"
+        tokens = torch.tensor([self.tokenizer.encode(x).ids], dtype=torch.long, requires_grad=False,
+                              device=self.device)
 
-        num_new_tokens = 0
-        tokens = torch.tensor(self.tokenizer.encode(x).ids, dtype=torch.long, device=self.device).unsqueeze(0)
-        while True:
-            logits, _ = self(tokens)
-            probs = F.softmax(logits[:, -1, :], dim=-1)
-            next_token_id = torch.argmax(probs, dim=-1)
-            tokens = torch.cat([tokens, next_token_id.unsqueeze(0)], dim=-1)
-            num_new_tokens += 1
-            if next_token_id==Tokens.eos_token or num_new_tokens>max_new_tokens:
-                break
+        while tokens[-1, -1].item() != Tokens.eos_token and tokens.size(-1)<max_len:
+
+            logits, _ = self(tokens) # B, T+1, VOCAB_SIZE
+            max_token_id = torch.argmax(logits[:, -1, :], dim=-1, keepdim=True).to(self.device)
+
+            tokens = torch.cat([tokens, max_token_id], dim=-1)
         
-
-        return self.tokenizer.decode(list(tokens.numpy()[0]))
+        op: str = self.tokenizer.decode(list(tokens.detach().cpu().numpy()[0]), skip_special_tokens=False)
+        sep_idx = op.find("<sep>")
+        predicted = op[sep_idx+5:]
+        if "<eos>" in predicted:
+            predicted = predicted[:-5]
+        return predicted.strip()
     
 
 class ParallelTranslateFormer(nn.Module):
@@ -90,22 +92,24 @@ class ParallelTranslateFormer(nn.Module):
         return logits, loss
     
     @torch.inference_mode()
-    def translate(self, x: str, max_new_tokens: int=20) -> str:
+    def translate(self, x: str, max_len: int=20) -> str:
         x = "<sos>" + x + "<sep>"
+        tokens = torch.tensor([self.tokenizer.encode(x).ids], dtype=torch.long, requires_grad=False,
+                              device=self.device)
 
-        num_new_tokens = 0
-        tokens = torch.tensor(self.tokenizer.encode(x).ids, dtype=torch.long, device=self.device).unsqueeze(0)
-        while True:
-            logits, _ = self(tokens)
-            probs = F.softmax(logits[:, -1, :], dim=-1)
-            next_token_id = torch.argmax(probs, dim=-1)
-            tokens = torch.cat([tokens, next_token_id.unsqueeze(0)], dim=-1)
-            num_new_tokens += 1
-            if next_token_id==Tokens.eos_token or num_new_tokens>max_new_tokens:
-                break
+        while tokens[-1, -1].item() != Tokens.eos_token and tokens.size(-1)<max_len:
+
+            logits, _ = self(tokens) # B, T+1, VOCAB_SIZE
+            max_token_id = torch.argmax(logits[:, -1, :], dim=-1, keepdim=True).to(self.device)
+
+            tokens = torch.cat([tokens, max_token_id], dim=-1)
         
-
-        return self.tokenizer.decode(list(tokens.numpy()[0]))    
+        op: str = self.tokenizer.decode(list(tokens.detach().cpu().numpy()[0]), skip_special_tokens=False)
+        sep_idx = op.find("<sep>")
+        predicted = op[sep_idx+5:]
+        if "<eos>" in predicted:
+            predicted = predicted[:-5]
+        return predicted.strip()
     
 
 class ConvTranslateFormer(nn.Module):
@@ -140,22 +144,24 @@ class ConvTranslateFormer(nn.Module):
         return logits, loss
     
     @torch.inference_mode()
-    def translate(self, x: str, max_new_tokens: int=20) -> str:
+    def translate(self, x: str, max_len: int=20) -> str:
         x = "<sos>" + x + "<sep>"
+        tokens = torch.tensor([self.tokenizer.encode(x).ids], dtype=torch.long, requires_grad=False,
+                              device=self.device)
 
-        num_new_tokens = 0
-        tokens = torch.tensor(self.tokenizer.encode(x).ids, dtype=torch.long, device=self.device).unsqueeze(0)
-        while True:
-            logits, _ = self(tokens)
-            probs = F.softmax(logits[:, -1, :], dim=-1)
-            next_token_id = torch.argmax(probs, dim=-1)
-            tokens = torch.cat([tokens, next_token_id.unsqueeze(0)], dim=-1)
-            num_new_tokens += 1
-            if next_token_id==Tokens.eos_token or num_new_tokens>max_new_tokens:
-                break
+        while tokens[-1, -1].item() != Tokens.eos_token and tokens.size(-1)<max_len:
+
+            logits, _ = self(tokens) # B, T+1, VOCAB_SIZE
+            max_token_id = torch.argmax(logits[:, -1, :], dim=-1, keepdim=True).to(self.device)
+
+            tokens = torch.cat([tokens, max_token_id], dim=-1)
         
-
-        return self.tokenizer.decode(list(tokens.numpy()[0]))
+        op: str = self.tokenizer.decode(list(tokens.detach().cpu().numpy()[0]), skip_special_tokens=False)
+        sep_idx = op.find("<sep>")
+        predicted = op[sep_idx+5:]
+        if "<eos>" in predicted:
+            predicted = predicted[:-5]
+        return predicted.strip()
     
 
 class PosTranslateFormer(nn.Module):
@@ -189,19 +195,21 @@ class PosTranslateFormer(nn.Module):
         return logits, loss
     
     @torch.inference_mode()
-    def translate(self, x: str, max_new_tokens: int=20) -> str:
+    def translate(self, x: str, max_len: int=20) -> str:
         x = "<sos>" + x + "<sep>"
+        tokens = torch.tensor([self.tokenizer.encode(x).ids], dtype=torch.long, requires_grad=False,
+                              device=self.device)
 
-        num_new_tokens = 0
-        tokens = torch.tensor(self.tokenizer.encode(x).ids, dtype=torch.long, device=self.device).unsqueeze(0)
-        while True:
-            logits, _ = self(tokens)
-            probs = F.softmax(logits[:, -1, :], dim=-1)
-            next_token_id = torch.argmax(probs, dim=-1)
-            tokens = torch.cat([tokens, next_token_id.unsqueeze(0)], dim=-1)
-            num_new_tokens += 1
-            if next_token_id==Tokens.eos_token or num_new_tokens>max_new_tokens:
-                break
+        while tokens[-1, -1].item() != Tokens.eos_token and tokens.size(-1)<max_len:
+
+            logits, _ = self(tokens) # B, T+1, VOCAB_SIZE
+            max_token_id = torch.argmax(logits[:, -1, :], dim=-1, keepdim=True).to(self.device)
+
+            tokens = torch.cat([tokens, max_token_id], dim=-1)
         
-
-        return self.tokenizer.decode(list(tokens.numpy()[0]))
+        op: str = self.tokenizer.decode(list(tokens.detach().cpu().numpy()[0]), skip_special_tokens=False)
+        sep_idx = op.find("<sep>")
+        predicted = op[sep_idx+5:]
+        if "<eos>" in predicted:
+            predicted = predicted[:-5]
+        return predicted.strip()