fa9e66d59b650761c37ea0c4534651f3e88b6c97
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2024-08-02 17:58:13 +0530

    moved to char level modelling

--- process_data.py
+++ process_data.py
@@ -1,122 +1,31 @@
-"""
-adapted from https://github.com/chaoluond/quicktypeGPT/blob/main/dataprocess.py
-"""
-
-import argparse
-import glob
-import os
-from concurrent.futures import ProcessPoolExecutor
-from functools import partial
-
+from datasets import load_dataset
 import numpy as np
-import sentencepiece as spm
-from tqdm import tqdm
+import os
 
-from tokenizer import Tokenizer
+ds = load_dataset('Salesforce/wikitext', 'wikitext-2-v1')
+train_txt, val_txt = '', ''
 
-DATA_CACHE_DIR = "data"
-TRAIN_MODE = "FIXED_BLOCK_SAMPLE"
+for item in ds['train']: train_txt += item['text']
+for item in ds['validation']: train_txt += item['text']
+for item in ds['test']: val_txt += item['text']
 
-
-def train_vocab(vocab_size):
-    """
-    Trains a custom sentencepiece tokenizer on the provided dataset.
-    The custom tokenizer files will be saved in DATA_CACHE_DIR/tok{N} directories,
-    where N is the vocab size. This is also where the pretok .bin files will go.
-    """
-    assert vocab_size > 0, "Vocab size must be positive"
-
-    # 1) input and output files prefix path for sentencepiece
-    prefix = os.path.join(DATA_CACHE_DIR, f"tok{vocab_size}")
-    input_file = os.path.join(DATA_CACHE_DIR, "train.txt")
-
-    print(f"Size is: {os.path.getsize(input_file) / 1024 / 1024:.2f} MB")
-
-    # 2) train the sentencepiece model
-    print("Will now train the vocab...")
-    spm.SentencePieceTrainer.train(input=input_file,
-                                   model_prefix=prefix,
-                                   model_type="bpe",
-                                   vocab_size=vocab_size,
-                                   self_test_sample_size=0,
-                                   input_format="text",
-                                   character_coverage=1.0,
-                                   num_threads=os.cpu_count(),
-                                   split_digits=True,
-                                   allow_whitespace_only_pieces=True,
-                                   byte_fallback=True,
-                                   unk_surface=r" \342\201\207 ",
-                                   normalization_rule_name="identity")
-
-    print("Done.")
+chars = sorted(list(set(train_txt + val_txt)))
+stoi = {ch:i for i, ch in enumerate(chars)}
+itos = {i:ch for ch, i in enumerate(chars)}
+encode_fn = lambda s: [stoi[ch] for ch in s]
+print(f'vocab size: {len(chars)}')
 
 
-def process_shard(args, vocab_size):
-    shard_id, shard = args
-    tokenizer_model = get_tokenizer_model_path(vocab_size)
-    enc = Tokenizer(tokenizer_model)
-    with open(shard, "r") as f:
-        data = [line for line in f]
-    all_tokens = []
-    for example in tqdm(data, position=shard_id):
-        text = example.strip()
-        tokens = enc.encode(text, bos=True, eos=False)  # encode the text, use BOS
-        all_tokens.extend(tokens)
-    # convert to uint16 nparray
-    all_tokens = np.array(all_tokens, dtype=np.uint16)
-    bin_dir = os.path.join(DATA_CACHE_DIR, f"tok{vocab_size}")
-    shard_basename = os.path.basename(shard)
-    bin_basename = shard_basename.replace(".txt", ".bin")
-    tokenized_filename = os.path.join(bin_dir, bin_basename)
-    # write the bytes
-    with open(tokenized_filename, "wb") as f:
-        f.write(all_tokens.tobytes())
-    # calculate the average sequence length (they are separated by BOS=1)
-    avg_seq_len = all_tokens.size / ((all_tokens == 1).sum())
-    print(f"Saved {tokenized_filename}, average seqlen: {avg_seq_len:.2f}")
+with open('data/train.txt', 'w') as f: f.write(train_txt)
+with open('data/val.txt', 'w') as f: f.write(val_txt)
 
+train_ids = encode_fn(train_txt)
+val_ids = encode_fn(val_txt)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
 
-def pretokenize(vocab_size):
-    # iterate the shards and tokenize all of them one by one in data/dialogue_all_data folder
-    shard_filenames = sorted(glob.glob(os.path.join(DATA_CACHE_DIR, "*.txt")))
-    
-    bin_dir = os.path.join(DATA_CACHE_DIR, f"tok{vocab_size}")
-    os.makedirs(bin_dir, exist_ok=True)
-
-    # process all the shards in a process pool
-    fun = partial(process_shard, vocab_size=vocab_size)
-    with ProcessPoolExecutor() as executor:
-        executor.map(fun, enumerate(shard_filenames))
-    print("Done.")
-
-
-# -----------------------------------------------------------------------------
-# public interface functions
-
-def get_tokenizer_model_path(vocab_size):
-    return os.path.join(DATA_CACHE_DIR, f"tok{vocab_size}.model")
-
-
-# -----------------------------------------------------------------------------
-# CLI for constructing the dataset
-
-if __name__ == "__main__":
-    """
-    These stages are designed to be run in order.
-
-    To tokenize data with a custom tokenizer we train ourselves with sentencepiece, e.g.:
-    python tinystories.py train_vocab --vocab_size=2048
-    python tinystories.py pretokenize --vocab_size=2048
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("stage", type=str, choices=["pretokenize", "train_vocab"])
-    parser.add_argument("--vocab_size", type=int, default=0, help="pretokenization vocab size. 0 = use Llama 2 tokenizer.")
-    args = parser.parse_args()
-
-    # depending on the stage call the appropriate function
-    if args.stage == "train_vocab":
-        train_vocab(vocab_size=args.vocab_size)
-    elif args.stage == "pretokenize":
-        pretokenize(vocab_size=args.vocab_size)
-    else:
-        raise ValueError(f"Unknown stage {args.stage}")
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join('data', 'train.bin'))
+val_ids.tofile(os.path.join('data', 'val.bin'))
