7769b282d3a2c13e44b3ee461b9597c6baaa2b99
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2024-11-05 12:23:35 +0800

    full gpt-2 size

--- models.py
+++ models.py
@@ -116,9 +116,9 @@ class ConvBlock(nn.Module):
 class GPTConfig:
     block_size: int = 1024 # max sequence length
     vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
-    n_layer: int = 8 # number of layers
-    n_head: int = 8 # number of heads
-    n_embd: int = 512 # embedding dimension
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
     dropout: float = 0.0
     bias = False
 
